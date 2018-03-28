622915c4f09f31b0ac8e02b10400ed4471c23f29
026 <026@192.168.0.64>
2018-03-28 16:55:09 +0900

    서블릿 모듈버전 2.x대

---
+++ ch11_servlet_intro_module_ver2.5/src/com/inc/servlet/HiServlet.java
@@ -0,0 +1,19 @@
+package com.inc.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HiServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		response.setContentType("text/html; charSet = UTF-8");
+		response.getWriter().print(name + " hi!");
+
+	}
+}
