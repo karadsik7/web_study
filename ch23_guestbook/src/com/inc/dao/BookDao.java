d18176ef1a45efdd4c6d1c2d07adb2b196f6a1a7
026 <026@192.168.0.14>
2018-04-27 17:52:02 +0900

    방명록 MyBatis 적용

--- ch23_guestbook/src/com/inc/dao/BookDao.java
+++ ch23_guestbook/src/com/inc/dao/BookDao.java
@@ -48,102 +48,36 @@ public class BookDao {
 	}
 	
 	public void del(int id) {
-		Connection conn = null;
-		Statement stmt = null;
-		try {
-			conn = DbConnector.getInstance().getConnection();
-			stmt = conn.createStatement();
-			stmt.executeUpdate(String.format("delete from guestbook where id = %d", id));
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
-			DbCloser.close(conn, stmt, null);
-		}
+		SqlSession session = factory.openSession(true);
+		session.delete("guestbook.del", id);
+		session.close();
 	}
 	
 	public BookVo selectOne(int id) {
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		BookVo bvo = new BookVo();
-		
-		try {
-			conn = DbConnector.getInstance().getConnection();
-			stmt = conn.createStatement();
-			String query = String.format("select * from guestbook where id = %d", id);
-			rs = stmt.executeQuery(query);
-			rs.next();
-			
-			bvo.setId(rs.getInt("id"));
-			bvo.setPassword(rs.getString("password"));
-			bvo.setMsg(rs.getString("msg"));
-			bvo.setWriter(rs.getString("writer"));
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
-			DbCloser.close(conn, stmt, rs);
-		}
-		return bvo;		
+		SqlSession session = factory.openSession();
+		BookVo bvo = session.selectOne("guestbook.selectOne", id);
+		session.close();
+		return bvo;
 	}
 	
 	public void modify(BookVo bvo) {
-		Connection conn = null;
-		Statement stmt = null;
-		try {
-			conn = DbConnector.getInstance().getConnection();
-			stmt = conn.createStatement();
-			int id = bvo.getId();
-			String writer = bvo.getWriter();
-			String password = bvo.getPassword();
-			String msg = bvo.getMsg();
-			
-			stmt.executeUpdate(String.format("update guestbook set writer = '%s', password = '%s', msg = '%s' where id = %d", writer, password, msg, id));
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
-			DbCloser.close(conn, stmt, null);
-		}
+		SqlSession session = factory.openSession(true);
+		session.update("guestbook.modify", bvo);
+		session.close();
 	}
 	
 	public String getPassword(int id) {
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		String password = null;
-		try {
-			conn = DbConnector.getInstance().getConnection();
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery(String.format("select password from guestbook where id = %d", id));
-			rs.next();
-			password = rs.getString("password");
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
-			DbCloser.close(conn, stmt, rs);
-		}
+		SqlSession session = factory.openSession();
+		String password = session.selectOne("guestbook.getPassword", id);
+		session.close();
 		return password;
 	}
 	
 	public int getWriterCheck(String writer) {
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
+		SqlSession session = factory.openSession();
 		int result = 404404;
-		try {
-			conn = DbConnector.getInstance().getConnection();
-			stmt = conn.createStatement();
-			String query = String.format("select count(*) as result from guestbook where writer = '%s'", writer);
-			rs = stmt.executeQuery(query);
-			rs.next();
-			result = rs.getInt("result");
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
-			DbCloser.close(conn, stmt, rs);
-		}
+		result = session.selectOne("guestbook.getWriterCheck", writer);
+		session.close();
 		return result;
 	}
 	
